b62eeb2c77f4edc23fb6d81000ed9e144e6e445e
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-05-01 09:00:56 +1000

    Created function for renaming files with no ascii characters to allow ingest.

---
+++ sip_file_renamer.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import logging
+from lxml import etree as ET
+
+## setup logging
+logger = logging.getLogger(__file__)
+format = '%(name)s - %(levelname)s : %(asctime)s - %(filename)s  - %(message)s'
+logging.basicConfig(filename="debug_sip_file_renamer.log", level=logging.DEBUG, format=format)
+
+
+def move_rename_files(root_directory, mets_filename="mets.xml"):
+    """A function that takes an existing SIP, checks for non-ascii filenames, creates a renamed
+    copy of those files in a renamed/ directory in streams and updates the href tag in the FLocat 
+    area of the METS manifest.
+    
+    Args: 
+        root_directory (path/str) : The location of the SIP. 
+        mets_filename (str) : Defaults to "mets.xml", but can be used to specify filename if one has been used."""
+    # Parse the mets from the file location.
+    mets_file = os.path.join(root_directory, "content", mets_filename)
+    mets = ET.parse(mets_file)
+
+    # Set the directory for renamed files.
+    rename_dir = os.path.join(root_directory,"content", "streams", 'renamed')
+
+    # Iterate through files and rename them if they are not ASCII. Update the href so that the file can be resolved.
+    for fl in mets.findall(".//{http://www.loc.gov/METS/}FLocat"):
+            fl_id = fl.getparent().attrib['ID']
+            root, ext = os.path.splitext(fl.attrib["{http://www.w3.org/1999/xlink}href"])
+            if not root.isascii():
+                logger.info(f"File: {root + ext} with file id {fl_id} contains non-ascii characters. File will be renamed.")
+                fl.attrib["{http://www.w3.org/1999/xlink}href"] = os.path.join("renamed",fl_id + ext)
+                logger.debug(f"METS updated for file {root+ext}")
+                if not os.path.exists(rename_dir):
+                     os.mkdir(rename_dir)
+                logger.info("Copying file to new location...")
+                try:
+                    shutil.copy2(os.path.join(root_directory, "content", "streams", root+ext), os.path.join(rename_dir, fl_id+ext))
+                    logger.info("File copied.")
+                except Exception as e:
+                    print(f"Error copying file: {root+ext}. Error message: {e}")
+                    logger.error(f"Error copying file: {root+ext}. Error message: {e}")
+    with open(mets_file, 'wb') as mf:
+         mf.write(ET.tostring(mets, xml_declaration=True, encoding='utf-8'))
+    return None
