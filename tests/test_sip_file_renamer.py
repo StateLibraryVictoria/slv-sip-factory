6508243b70ed2467df3c30b0ac4359d17e8b2969
Susannah Bourke <sbourke@slv.vic.gov.au>
2024-05-09 11:08:19 +1000

    Updated tests for new function.

--- tests/test_sip_file_renamer.py
+++ tests/test_sip_file_renamer.py
@@ -8,17 +8,13 @@ from rosetta_sip_factory import sip_builder as sb
 from sip_file_renamer import *
 
 # Setup test data.
-input_path = os.path.join("tests","test_data","input")
+input_path = os.path.join("tests", "test_data", "input")
 sip_list = os.listdir(input_path)
-print(sip_list)
-output_path = os.path.join("tests","test_data","output")
-output_renamed_path = os.path.join("tests","test_data","output_renamed")
-percent_encoding_path = os.path.join("tests","test_data","percent_encoding")
-"""validation_schema_file = os.path.join("tests", "test_data", "mets_schema.xsd")
-with open(validation_schema_file, 'r', encoding='utf-8') as file:
-     xml_schema_doc = ET.parse(file)
-     xmlschema = ET.XMLSchema(xml_schema_doc)
-"""
+output_path = os.path.join("tests", "test_data", "output")
+output_renamed_path = os.path.join("tests", "test_data", "output_renamed")
+percent_encoding_path = os.path.join("tests", "test_data", "percent_encoding")
+
+
 # test based on rosetta_sip_factory_test
 @pytest.mark.parametrize("input_folder", [sip_list[0], sip_list[1]])
 def test_mets_dnx(input_folder):
@@ -32,26 +28,47 @@ def test_mets_dnx(input_folder):
     # first off, delete anything that's in the output folder
     shutil.rmtree(output_dir)
     os.makedirs(output_dir)
-    ie_dc_dict = {"dc:title": title}
     sb.build_sip(
-        ie_dmd_dict=ie_dc_dict,
+        ie_dmd_dict=[
+            {
+                "dc:title": title,  # title of IE, becomes dc:alternative once split.
+                "rosetta:externalId": pi,
+                "rosetta:externalSystem": "ArchivesSpace",
+            }
+        ],
         pres_master_dir=input_dir,
         input_dir=input_dir,
         generalIECharacteristics=[
-                {'submissionReason': 'bornDigitalContent',
-                 'IEEntityType': 'periodicIE'}
-                ],
-        output_dir=output_dir
-        )
+            {"submissionReason": "bornDigitalContent", "IEEntityType": "periodicIE"}
+        ],
+        sip_title=title,
+        output_dir=output_dir,
+    )
+    files = os.listdir(os.path.join(output_dir, "content"))
+    files.sort()
+    expected = ["mets.xml", "dc.xml", "streams"]
+    expected.sort()
+    print(files)
+    assert files == expected
+
 
 # create a function that updates the filename href for output in output folder.
-expected_names_1 = ['fid1-1.doc', 'fid2-1.xlsx', 'fid3-1.doc', 'fid4-1.msg']
-expected_names_2 = ['fid2-1.doc', 'fid3-1.xlsx', 'fid4-1.doc','fid5-1.jpg','fid6-1.msg']
+expected_names_1 = ["fid1-1.doc", "fid2-1.xlsx", "fid3-1.doc", "fid4-1.msg"]
+expected_names_2 = [
+    "fid2-1.doc",
+    "fid3-1.xlsx",
+    "fid4-1.doc",
+    "fid5-1.jpg",
+    "fid6-1.msg",
+]
 expected_names_1.sort()
 expected_names_2.sort()
 
 
-@pytest.mark.parametrize("input_folder, expected", [(sip_list[0], expected_names_1), (sip_list[1], expected_names_2)])
+@pytest.mark.parametrize(
+    "input_folder, expected",
+    [(sip_list[0], expected_names_1), (sip_list[1], expected_names_2)],
+)
 def test_move_renamed_files(input_folder, expected):
     # create the renamed data from the output folder data.
     output_dir = os.path.join(output_path, input_folder)
@@ -71,27 +88,34 @@ def test_move_renamed_files(input_folder, expected):
     move_rename_files(renamed_output)
 
     # list files in renamed directory to check against expected
-    renamed_files = os.listdir(os.path.join(renamed_output,'content','streams','renamed'))
+    renamed_files = os.listdir(
+        os.path.join(renamed_output, "content", "streams", "renamed")
+    )
     renamed_files.sort()
-    
-    assert(expected == renamed_files)
+
+    assert expected == renamed_files
 
     # Checks the updated manifest.
-@pytest.mark.parametrize("input_folder, expected", [(sip_list[0], expected_names_1), (sip_list[1], expected_names_2)])
-def test_updated_manifest_for_renamed_files(input_folder, expected):    
+
+
+@pytest.mark.parametrize(
+    "input_folder, expected",
+    [(sip_list[0], expected_names_1), (sip_list[1], expected_names_2)],
+)
+def test_updated_manifest_for_renamed_files(input_folder, expected):
     renamed_output = os.path.join(output_renamed_path, input_folder)
     mets_file = os.path.join(renamed_output, "content", "mets.xml")
     mets = ET.parse(mets_file)
     updated_list = []
     for fl in mets.findall(".//{http://www.loc.gov/METS/}FLocat"):
-            fl_id = fl.getparent().attrib['ID']
-            path, name = os.path.split(fl.attrib["{http://www.w3.org/1999/xlink}href"])
-            print(path)
-            print(name)
-            if "renamed" in path:
-                 print("renamed in path")
-                 updated_list.append(name)
-    assert(updated_list == expected)
+        fl_id = fl.getparent().attrib["ID"]
+        path, name = os.path.split(fl.attrib["{http://www.w3.org/1999/xlink}href"])
+        print(path)
+        print(name)
+        if "renamed" in path:
+            print("renamed in path")
+            updated_list.append(name)
+    assert updated_list == expected
 
 
 # Test no files have gone missing.
@@ -103,11 +127,12 @@ def test_all_files_are_in_both_locations(input_folder):
         numInput = len(files)
     for root, dirs, files in os.walk(renamed_output):
         numOutput = len(files)
-    assert(numInput == numOutput)
+    assert numInput == numOutput
+
 
 def test_percent_encoding_for_ascii_filenames():
     input_dir = percent_encoding_path
-    output_dir = os.path.join("tests","test_data","percent_output")
+    output_dir = os.path.join("tests", "test_data", "percent_output")
     title = "Test percent encoding"
 
     # first off, delete anything that's in the output folder
@@ -120,18 +145,53 @@ def test_percent_encoding_for_ascii_filenames():
         pres_master_dir=input_dir,
         input_dir=input_dir,
         generalIECharacteristics=[
-                {'submissionReason': 'bornDigitalContent',
-                 'IEEntityType': 'periodicIE'}
-                ],
-        output_dir=output_dir
-        )
+            {"submissionReason": "bornDigitalContent", "IEEntityType": "periodicIE"}
+        ],
+        output_dir=output_dir,
+    )
     move_rename_files(output_dir)
     mets_file = os.path.join(output_dir, "content", "mets.xml")
     mets = ET.parse(mets_file)
-    expected = ["New%20Text%20Document.txt", "something%25sign/This%20That.txt", "something%25sign/This%2520That.txt"]
+    expected = [
+        "New%20Text%20Document.txt",
+        "something%25sign/This%20That.txt",
+        "something%25sign/This%2520That.txt",
+    ]
     output = []
     for fl in mets.findall(".//{http://www.loc.gov/METS/}FLocat"):
-            output.append(fl.attrib["{http://www.w3.org/1999/xlink}href"])
+        output.append(fl.attrib["{http://www.w3.org/1999/xlink}href"])
     print(output)
-    assert(expected == output)
+    assert expected == output
 
+
+# Test dc.xml has been created
+@pytest.mark.parametrize("input_folder", [sip_list[0], sip_list[1]])
+def test_dc_xml_in_output(input_folder):
+    dcxml = os.path.join(output_renamed_path, input_folder, "content", "dc.xml")
+    assert os.path.isfile(dcxml) == True
+
+
+# Test rosetta:externalId in dc.xml
+@pytest.mark.parametrize(
+    "input_folder, expected",
+    [
+        (
+            sip_list[0],
+            '<dc:record xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:rosetta="http://www.exlibrisgroup.com/dps"><rosetta:externalId>test-add-pi-to-xml</rosetta:externalId><rosetta:externalSystem>test_system</rosetta:externalSystem><dc:title>test_sip_metadata_test-SIP_An_example_sip_20240430</dc:title></dc:record>',
+        ),
+        (
+            sip_list[1],
+            '<dc:record xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:rosetta="http://www.exlibrisgroup.com/dps"><rosetta:externalId>test-add-pi-to-xml</rosetta:externalId><rosetta:externalSystem>test_system</rosetta:externalSystem><dc:title>test_sip_metadata_test-SIP_A_sip_with_hierarchy_20240430</dc:title></dc:record>',
+        ),
+    ],
+)
+def test_generate_dcxml(input_folder, expected):
+    record = generate_dcxml_record(
+        "test-add-pi-to-xml",
+        "test_system",
+        "test_sip_metadata_" + input_folder,
+    )
+    record = ET.tostring(record).decode()
+    print(record)
+    print(expected)
+    assert record == expected
